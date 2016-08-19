8626463bfd22d61c61f5683259b1f3462d61d8f2
rahulyadav20111995 <rahulyadav574@gmail.com>
2016-08-20 00:04:15 +0530

    Text to speech added

--- app/src/main/java/in/nfcstarter/Fragments/About.java
+++ app/src/main/java/in/nfcstarter/Fragments/About.java
@@ -1,34 +1,28 @@
 package in.nfcstarter.Fragments;
 
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.google.gson.JsonObject;
-import com.squareup.okhttp.Callback;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.RequestBody;
-import com.squareup.okhttp.Response;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.Text;
 
-import java.io.IOException;
+import java.util.Locale;
 
 import in.nfcstarter.Constants;
 import in.nfcstarter.DescriptionActivity;
-import in.nfcstarter.LandingActivity;
 import in.nfcstarter.R;
 import in.nfcstarter.Utility;
 
@@ -41,11 +35,16 @@ public class About extends Fragment {
     private TextView queryRef;
     private TextView queryInfo;
     private static final String TAG = "AboutFragment";
+    private String summaryForSpeech;
+    private TextToSpeech textToSpeech;
 
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        this.setHasOptionsMenu(true);
+
     }
 
     @Nullable
@@ -57,9 +56,15 @@ public class About extends Fragment {
         querySummary = (TextView) view.findViewById(R.id.querySummary);
         queryRef = (TextView) view.findViewById(R.id.queryref);
 
-
-
         return view;
+    }
+
+    public void onPause(){
+        if(textToSpeech !=null){
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+        super.onPause();
     }
 
     @Override
@@ -109,6 +114,7 @@ public class About extends Fragment {
 
                 JSONObject jsonObject = jsonObject1.getJSONArray("documents").getJSONObject(0);
                 final String summary = jsonObject.getString("summary");
+                summaryForSpeech = summary;
                 final String ref = jsonObject.getString("reference");
 
                 JSONArray jsonArray = jsonObject.getJSONArray("wikipedia_category");
@@ -145,4 +151,34 @@ public class About extends Fragment {
             dialog.cancel();
         }
     }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.fragment_about_menu,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.convertTextToSpeech){
+            convertSummaryToSpeech();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void convertSummaryToSpeech(){
+
+      //  textToSpeech.speak(summaryForSpeech, TextToSpeech.QUEUE_FLUSH, null);
+
+        textToSpeech  = new TextToSpeech(getActivity(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    textToSpeech.setLanguage(Locale.UK);
+                    textToSpeech.speak(summaryForSpeech,TextToSpeech.QUEUE_FLUSH,null,"574");
+                }
+            }
+        });
+
+}
 }
